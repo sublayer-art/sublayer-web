e7b6f6143b4460c55cc658b310cc9ea845ce5d61
unknown <tobilon@outlook.com>
2024-12-09 12:17:39 +0800

    change banner

--- src/pages/trade/Banner.tsx
+++ src/pages/trade/Banner.tsx
@@ -14,7 +14,7 @@ import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 
-const items = ["/banner-2.png", "/banner-1.jpg"];
+const items = ["/banner-2.png", "/banner-1.png"];
 const Banner: React.FC = () => {
   function PrevArrow(props: any) {
     return (
